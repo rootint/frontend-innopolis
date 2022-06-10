f8e0fe33f1785a4f284c849a0772e6998ce53151
RandoM <danik.tim@gmail.com>
2022-06-10 23:25:44 +0300

    add quote of the day and redone grid

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 var code_lines = document.getElementById("codelines");
+var code_snippet = document.getElementById("codesnippet");
 var lines = "";
-for (var i = 1; i < 120; ++i) {
-  lines += i + "<br>"
+for (var i = 1; i < 100; ++i) {
+  lines += i + "<br>";
 }
 code_lines.innerHTML = lines;
+
+window.onload = async function () {
+  var response = await (await fetch("https://favqs.com/api/qotd")).json();
+  console.log(response["quote"]);
+  code_snippet.innerText = "Quote of the day: \n\n";
+  code_snippet.innerText += JSON.stringify(response["quote"]["body"]) + '\n\n';
+  code_snippet.innerText += JSON.stringify(response["quote"]["author"]);
+};
